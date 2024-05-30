7f208527d71a7bf53109ddd4ebdec0d974263535
vinaykumar adepu <vinaykumar.adepu456@gmail.com>
2024-05-30 09:30:48 +0530

    carassignment

---
+++ src/main/Car.java
@@ -0,0 +1,4 @@
+public class Car {
+  String carBrand;
+  int noOfGears;
+}
---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    Car c1=new Car();
+    c1.carBrand="tata";
+    c1.noOfGears=5;
+    print();
+  }
+  public static void print(){
+    System.out.println("car brand name is: "+c1.carBrand);
+    System.out.println("no of gears car had is: "+noOfGears);
+  }
+}
